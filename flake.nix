40dda57ea9ca2305ddc96bedef7047c9183399df
omi-coide <74772084+omi-coide@users.noreply.github.com>
2023-03-02 20:42:33 +0800

    add pkg-config search path for ns3

--- flake.nix
+++ flake.nix
@@ -11,14 +11,14 @@
 
   };
 
-  outputs = { self, nixpkgs, ... } @ inputs:
+  outputs = { self, nixpkgs, ylynur, ... } @ inputs:
     let
       system = "x86_64-linux";
     in
     {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
       rust = import ./rust.nix { inherit inputs system; };
-      ns3 = import ./ns3.nix { inherit inputs system; };
+      ns3 = import ./ns3.nix { inherit inputs system; NetAnim = ylynur.packages.${system}.NetAnim; };
     };
 }
 
--- ns3.nix
+++ ns3.nix
@@ -1,13 +1,16 @@
-{ inputs, system, ... }:
+{ inputs, system,  ... }:
 let
   mkShell = inputs.nixpkgs.legacyPackages.${system}.mkShell;
   pkgs = inputs.nixpkgs.legacyPackages.${system};
+  nur = inputs.ylynur.packages.${system};
 in
 mkShell {
-  packages = with pkgs;[ ns-3 pkg-config ];
+  packages = with pkgs;[ ns-3 pkg-config nur.NetAnim ];
   C_INCLUDE_PATH = "${pkgs.ns-3}/include/ns3.35";
   CPLUS_INCLUDE_PATH = "${pkgs.ns-3}/include/ns3.35";
   LD_LIBRARY_PATH = "${pkgs.ns-3}/lib"; # runtime search path
   LIBRARY_PATH = "${pkgs.ns-3}/lib"; # buildtime search path
+  PKG_CONFIG_PATH = "${pkgs.ns-3}/lib/pkgconfig"; # pkgconfig search path
 }
+
 
