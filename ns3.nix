c18a4de1844c2379b48faba2fde7428cd97bf76e
omi-coide <74772084+omi-coide@users.noreply.github.com>
2023-03-02 20:20:31 +0800

    add LIBRARY_PATH for develop usage

--- ns3.nix
+++ ns3.nix
@@ -4,9 +4,10 @@ let
   pkgs = inputs.nixpkgs.legacyPackages.${system};
 in
 mkShell {
-  packages = with pkgs;[ ns-3 ];
+  packages = with pkgs;[ ns-3 pkg-config ];
   C_INCLUDE_PATH = "${pkgs.ns-3}/include/ns3.35";
   CPLUS_INCLUDE_PATH = "${pkgs.ns-3}/include/ns3.35";
-  LD_LIBRARY_PATH = "${pkgs.ns-3}/lib";
+  LD_LIBRARY_PATH = "${pkgs.ns-3}/lib"; # runtime search path
+  LIBRARY_PATH = "${pkgs.ns-3}/lib"; # buildtime search path
 }
 
